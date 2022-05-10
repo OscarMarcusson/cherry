a53897be3a392756c29b34613a75db141f1a8a84
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2022-05-10 16:51:22 +0200

    Refactored into separate classes

---
+++ compiler/Simplified User Interface Framework/Intermediate/Style/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplifiedUserInterfaceFramework.Intermediate
+{
+	public enum DisplayLimit
+	{
+		Ignored = 0,
+		Screen = 1 << 1,
+		Print = 1 << 2,
+		Voice = 1 << 3,
+	}
+}
