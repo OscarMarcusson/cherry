ca148f275d914dbe575175b1f3bdc194cb61eb6d
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2022-06-08 08:40:51 +0200

    Added the variable cache

---
+++ compiler/Simplified User Interface Framework/Intermediate/Script/VariablesCache.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplifiedUserInterfaceFramework.Intermediate
+{
+	public class VariablesCache
+	{
+		readonly object Locker = new object();
+		readonly Dictionary<string, Variable> Variables = new Dictionary<string, Variable>();
+
+		const string NullGetWarning = "Can't get a variable using null as a key";
+		const string NullSetWarning = "Can't set a variable using null as a key";
+
+
+		public Variable this[string key]
+		{
+			get
+			{
+				if (key == null)
+					throw new ArgumentNullException(NullGetWarning);
+
+				lock (Locker)
+				{
+					if (Variables.TryGetValue(key, out var value))
+						return value;
+
+					return null;
+				}
+			}
+			set
+			{
+				if (key == null)
+					throw new ArgumentNullException(NullSetWarning);
+
+				lock (Locker)
+				{
+					if(value == null)
+						Variables.Remove(key);
+				
+					else
+						Variables[key] = value;
+				}
+			}
+		}
+
+
+		public bool TryGetVariable(string key, out Variable variable)
+		{
+			if (key == null)
+				throw new ArgumentNullException(NullGetWarning);
+
+			lock (Locker)
+				return Variables.TryGetValue(key, out variable);
+		}
+
+
+		public bool Exists(string key)
+		{
+			if (key == null)
+				throw new ArgumentNullException(NullGetWarning);
+
+			lock (Locker)
+			{
+				return Variables.ContainsKey(key);
+			}
+		}
+	}
+}
---
+++ compiler/Tests/Variables/VariablesCacheTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplifiedUserInterfaceFramework.Intermediate;
+
+namespace Variables
+{
+	[TestClass]
+	public class VariablesCacheTests
+	{
+		[TestMethod]
+		public void CanUseIndex()
+		{
+			var cache = new VariablesCache();
+			
+			var a = new Variable(VariableType.ReadOnly, "a", "5");
+			cache["a"] = a;
+			Assert.AreEqual(a, cache["a"]);
+		}
+
+
+		[TestMethod]
+		public void CanUseTryGet()
+		{
+			var cache = new VariablesCache();
+
+			var a = new Variable(VariableType.ReadOnly, "a", "5");
+			cache["a"] = a;
+
+			Assert.AreEqual(true, cache.TryGetVariable("a", out var foundA));
+			Assert.AreEqual(a, foundA);
+
+			Assert.AreEqual(false, cache.TryGetVariable("b", out var foundB));
+			Assert.AreEqual(null, foundB);
+		}
+
+
+		[TestMethod]
+		public void CanUseExists()
+		{
+			var cache = new VariablesCache();
+
+			cache["a"] = new Variable(VariableType.ReadOnly, "a", "5");
+
+			Assert.AreEqual(true, cache.Exists("a"));
+			Assert.AreEqual(false, cache.Exists("b"));
+		}
+
+
+		[TestMethod]
+		public void CanRemove()
+		{
+			var cache = new VariablesCache();
+
+			var a = new Variable(VariableType.ReadOnly, "a", "5");
+			cache["a"] = a;
+			cache["a"] = null;
+
+			Assert.AreEqual(null, cache["a"]);
+			Assert.AreEqual(false, cache.Exists("a"));
+		}
+	}
+}
