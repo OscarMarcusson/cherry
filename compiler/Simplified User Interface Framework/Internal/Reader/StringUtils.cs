641a0755835559dcbbaf1ed2781e83c58f80b682
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2022-06-19 19:23:07 +0200

    Added a utility class for reading words

---
+++ compiler/Simplified User Interface Framework/Internal/Reader/StringUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("UnitTests")]
+namespace SimplifiedUserInterfaceFramework
+{
+	internal static class StringUtils
+	{
+		public static string GetNextWord(this string s, ref int index)
+		{
+			if (index >= s.Length)
+				return null;
+
+			var nextSpace = s.IndexOf(' ', index);
+			if (nextSpace < 0)
+			{
+				var remainingWord = s.Substring(index);
+				index = s.Length;
+				return remainingWord;
+			}
+
+			var word = s.Substring(index, nextSpace - index);
+			index = nextSpace;
+			while (index < s.Length && s[index] == ' ')
+				index++;
+
+			return word;
+		}
+	}
+}
---
+++ compiler/Tests/Utils/StringUtilsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplifiedUserInterfaceFramework;
+
+namespace Utils
+{
+	[TestClass]
+	public class StringUtilsTest
+	{
+		[TestMethod]
+		public void GetFirstWord()
+		{
+			var s = "Hello World";
+			var index = 0;
+			var firstWord = s.GetNextWord(ref index);
+
+			Assert.AreEqual("Hello", firstWord);
+		}
+
+		[TestMethod]
+		public void IndexIsMovedToStartOfNextWord()
+		{
+			var s = "Hello   World";
+			var index = 0;
+
+			_ = s.GetNextWord(ref index);
+			Assert.AreEqual(8, index);
+		}
+
+		[TestMethod]
+		public void GetRemainderOfWord()
+		{
+			var s = "Hello World";
+			var index = 2;
+			var firstWord = s.GetNextWord(ref index);
+
+			Assert.AreEqual("llo", firstWord);
+
+			index = 6;
+			var secondWord = s.GetNextWord(ref index);
+			Assert.AreEqual("World", secondWord);
+		}
+	}
+}
