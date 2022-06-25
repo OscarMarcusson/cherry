1e28d81c67d7940a4edebefa845457c21c2b83be
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2022-06-25 13:19:47 +0200

    WIP tests

---
+++ compiler/Tests/Functions/IfElseStatements.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Functions
+{
+	[TestClass]
+	public class IfElseStatements
+	{
+		[TestMethod]
+		public void DetectsCorrectKey()
+		{
+			// TODO::
+			// "if"
+			// "else if"
+			// "else"
+			// "askdjasdjsakad" << error
+			// uppercase << error
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void ParsesCondition()
+		{
+			// if >>>everything_here<<<
+			// same for else if & else
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void HasCorrectChildren()
+		{
+			// if a > 2
+			//     print("test")
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void OneLiner()
+		{
+			// if a > 2; print("yes")
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void CompileTimeResolveLiterals()
+		{
+			// will never call print
+			// if 1 > 2; print("nope")
+
+			// will be converted to just the print call
+			// if true; print("yes")
+			throw new NotImplementedException();
+		}
+	}
+}
